a7f54576f19c77c2183702b97849d78f0c76ef00
lizhaobo <izb2008@163.com>
2017-08-22 22:08:15 +0800

    modify ajax error

--- src/main/java/controller/HomelistController.java
+++ src/main/java/controller/HomelistController.java
@@ -1,9 +1,12 @@
 package controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import model.MyRunnable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import utils.HttpUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,21 +19,146 @@ public class HomelistController {
 
 //    private static final Logger logger = (Logger) LoggerFactory.getLogger(HomeController.class);
 
-    @RequestMapping("/BTC")
-    @ResponseBody
-    public Map<String,Object> login(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    @RequestMapping(value="/BTC", produces="application/json;charset=UTF-8")
+    public @ResponseBody Map<String,Object> login(HttpServletRequest request, HttpServletResponse response) throws IOException {
         System.out.println(request.getParameter("name"));
         Map<String,Object> map = new HashMap<String,Object>();
 
-//        if(request.getParameter("name").equals("123")){
-//            System.out.println("城东");
-//            map.put("msg", "成功");
-//        }else{
-            System.out.println("失败");
-            map.put("msg", "失败");
-//        }
+        List<Map<String, Object>> list = Collections.synchronizedList(new ArrayList<Map<String, Object>>());
+        List<Map<String, Object>> listNoValue = Collections.synchronizedList(new ArrayList<Map<String, Object>>());
+
+        Thread t1 = new Thread(new MyRunnable(list, listNoValue) {
+            @Override
+            public void run() {
+                HashMap<String, Object> map = get("http://api.huobi.com/staticmarket/ticker_btc_json.js", "火币", "ticker", "last");
+                if (map.get("flag") != null) {
+                    map.put("price", "暂无数据");
+                    map.put("percent", "暂无数据");
+                    map.put("difference", "暂无数据");
+                    listNoValue.add(map);
+                } else {
+                    list.add(map);
+                }
+            }
+        });
+        t1.start();
+
+        Thread t2 = new Thread(new MyRunnable(list, listNoValue) {
+            @Override
+            public void run() {
+                HashMap<String, Object> map = get("https://www.okcoin.cn/api/v1/ticker.do?symbol=btc_cny", "okcoin", "ticker", "last");
+                if (map.get("flag") != null) {
+                    map.put("price", "暂无数据");
+                    map.put("percent", "暂无数据");
+                    map.put("difference", "暂无数据");
+                    listNoValue.add(map);
+                } else {
+                    list.add(map);
+                }
+            }
+        });
+        t2.start();
+
+        Thread t3 = new Thread(new MyRunnable(list, listNoValue) {
+            @Override
+            public void run() {
+                HashMap<String, Object> map = get("https://api.bithumb.com/public/ticker/BTC", "bithumb", "data", "buy_price");
+                if (map.get("flag") != null) {
+                    map.put("price", "暂无数据");
+                    map.put("percent", "暂无数据");
+                    map.put("difference", "暂无数据");
+                    listNoValue.add(map);
+                } else {
+                    map.put("price", HttpUtils.keepDecimal((Double) map.get("price") * TaskDemo.getKRW_CNY(), 3));
+                    list.add(map);
+                }
+            }
+        });
+        t3.start();
+
+        Thread t4 = new Thread(new MyRunnable(list, listNoValue) {
+            @Override
+            public void run() {
+                HashMap<String, Object> map = get("https://api.bitflyer.jp/v1/ticker", "bitflyer", "ltp", null);
+                if (map.get("flag") != null) {
+                    map.put("price", "暂无数据");
+                    map.put("percent", "暂无数据");
+                    map.put("difference", "暂无数据");
+                    listNoValue.add(map);
+                } else {
+                    map.put("price", HttpUtils.keepDecimal((Double) map.get("price") * TaskDemo.getJPY_CNY(), 3));
+                    list.add(map);
+                }
+            }
+        });
+        t4.start();
+
+        Thread t5 = new Thread(new MyRunnable(list, listNoValue) {
+            @Override
+            public void run() {
+                HashMap<String, Object> map = get("https://data.btcchina.com/data/ticker?market=btccny", "btcChina", "ticker", "last");
+                if (map.get("flag") != null) {
+                    map.put("price", "暂无数据");
+                    map.put("percent", "暂无数据");
+                    map.put("difference", "暂无数据");
+                    listNoValue.add(map);
+                } else {
+                    map.put("price", HttpUtils.keepDecimal((Double) map.get("price"), 3));
+                    list.add(map);
+                }
+            }
+        });
+        t5.start();
+
+        while (t1.isAlive() || t2.isAlive() || t3.isAlive() || t4.isAlive() || t5.isAlive()) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Collections.sort(list, new Comparator<Map<String, Object>>() {
+            @Override
+            public int compare(Map<String, Object> arg0, Map<String, Object> arg1) {
+                Double d1 = Double.parseDouble(arg0.get("price").toString());
+                Double d2 = Double.parseDouble(arg1.get("price").toString());
+                return d1 > d2 ? 1 : -1;
+            }
+        });
+
+        if (!list.isEmpty()) {
+            Double price = (Double) list.get(0).get("price");
+            for (Map m : list) {
+                Double difference = (Double)m.get("price") - price;
+                m.put("percent", (HttpUtils.keepDecimal((difference/price) * 100, 3) + "%"));
+                m.put("difference", HttpUtils.keepDecimal(difference, 3));
+            }
+        }
+
+        System.out.println(list);
+        map.put("list", list);
+        map.put("listNoValue", listNoValue);
+        map.put("name", "BTC");
         return map;
     }
 
+    public HashMap<String, Object> get(String url, String name, String param1, String param2) {
+        JSONObject json = HttpUtils.get(url);
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put("name", name);
+        String str;
+        if (json == null) {
+            map.put("flag", "1");
+            return map;
+        }
 
+        if (param2 != null) {
+            str = (JSON.parseObject(json.getString(param1)).getString(param2)).split(",")[0].replace("[", "");
+        } else {
+            str = json.getString(param1);
+        }
+        map.put("price", HttpUtils.keepDecimal(str, 3));
+        return map;
+    }
 }
--- src/main/webapp/WEB-INF/views/new.jsp
+++ src/main/webapp/WEB-INF/views/new.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
          pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%
     String path = request.getContextPath();
     String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";
@@ -10,6 +11,53 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>Insert title here</title>
     <script type="text/javascript" src="<%=basePath%>js/jquery-1.8.1.js"></script>
+    <script type="text/javascript">
+        $("#btnId_1").click(function(){
+            $.ajax({
+                type: "GET",
+                url: "http://localhost:8080/btc/homelist/BTC",
+//            contentType:"application/json;charset=utf-8",
+                dataType:"json",
+                success: function(data){
+                    alert( "msg : " + data.list );
+                    var array = new Array();
+
+//2、通过c标签循环sinList 并生成添加到array的字符串
+                    <c:forEach items="${data.list}" var="a">
+                    array.push(${a}); //生成如 array.push(123)的字符串 这样前台拿到后就是js
+                    </c:forEach>
+                },
+                error:
+                        function(XMLHttpRequest, textStatus, errorThrown) {
+                            alert(XMLHttpRequest.status);
+                            alert(XMLHttpRequest.readyState);
+                            alert(textStatus);
+                        }
+            });
+        });
+
+        $("#btnId_2").click(function(){
+            $.ajax({
+                url: "http://127.0.0.1:8080/springMVC_demo_ano/user/login",
+                type:"POST",
+                cache:false,
+                headers:{
+                    'Content-Type':'application/json',
+                    'Accept':'application/json'
+                },
+                dataType:"json",
+                data: JSON.stringify({
+                    'account' : "aaa",
+                    'password' : "bbb"
+                }),
+                error: function(){},
+                success: function(data){
+                    alert("data="+data.msg);
+                }
+            });
+        });
+
+    </script>
 </head>
 <body>
 <p>
@@ -18,42 +66,52 @@
 <p>
     <button id="btnId_2">ajax请求2</button>
 </p>
+<table id="travel" summary="Travel times to work by main mode (Autumn 2006) - Source: London Travel Report 2007 http://www.tfl.gov.uk/assets/downloads/corporate/London-Travel-Report-2007-final.pdf">
+
+    <caption>Travel patterns in London - Travel times to work by main mode (Autumn 2006) - Minutes</caption>
+
+    <thead>
+    <tr>
+        <th scope="col" rowspan="2">${name}</th>
+        <%--<th scope="col" colspan="6">Area of workplace</th>--%>
+    </tr>
+
+    <tr>
+        <th scope="col">金额（CNY）</th>
+        <th scope="col">相差百分比</th>
+        <th scope="col">差额</th>
+        <%--<th scope="col">All London</th>--%>
+        <%--<th scope="col">Rest of Great Britain</th>--%>
+        <%--<th scope="col">Great Britain</th>--%>
+    </tr>
+    </thead>
+
+    <%--<tfoot>--%>
+    <%--<tr>--%>
+    <%--<th scope="row">All modes</th>--%>
+    <%--<td>55</td>--%>
+    <%--<td>39</td>--%>
+    <%--<td>27</td>--%>
+    <%--<td>39</td>--%>
+    <%--<td>20</td>--%>
+    <%--<td>23</td>--%>
+    <%--</tr>--%>
+    <%--</tfoot>--%>
+
+    <tbody>
+    <c:forEach items="${array}" var="list"  varStatus="i">
+        <%--<li><p>${list.name} : ${list.price}</p></li>--%>
+        <tr>
+            <th scope="row">${list.name}</th>
+            <td>${list.price}</td>
+            <td>${list.percent}</td>
+            <td>${list.difference}</td>
+        </tr>
+    </c:forEach >
+
+    </tbody>
+
+</table>
 </body>
-<script type="text/javascript">
-    $("#btnId_1").click(function(){
-        $.ajax({
-            type: "GET",
-            url: "http://localhost:8080/btc/homelist/BTC",
-            success: function(msg){
-                alert( "msg : " + msg );
-            },
-            error:
-                function(msg){
-                    alert( "error : " + msg );
-                }
-        });
-    });
 
-    $("#btnId_2").click(function(){
-        $.ajax({
-            url: "http://127.0.0.1:8080/springMVC_demo_ano/user/login",
-            type:"POST",
-            cache:false,
-            headers:{
-                'Content-Type':'application/json',
-                'Accept':'application/json'
-            },
-            dataType:"json",
-            data: JSON.stringify({
-                'account' : "aaa",
-                'password' : "bbb"
-            }),
-            error: function(){},
-            success: function(data){
-                alert("data="+data.msg);
-            }
-        });
-    });
-
-</script>
 </html>
